8f63918e79bd91a2df848aaa023e44cd359438dd
temporizer <temporizer@gmail.com>
2014-05-11 00:18:22 -0700

    update ctec227_final.sql

--- ctec227_final.sql
+++ ctec227_final.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: May 11, 2014 at 08:04 AM
+-- Generation Time: May 11, 2014 at 09:17 AM
 -- Server version: 5.5.34
 -- PHP Version: 5.5.9
 
@@ -115,20 +115,9 @@ CREATE TABLE IF NOT EXISTS `task` (
   `task_name` varchar(255) NOT NULL,
   `description` mediumtext NOT NULL,
   `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `status_id` int(11) NOT NULL,
   PRIMARY KEY (`task_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `task_user`
---
-
-DROP TABLE IF EXISTS `task_user`;
-CREATE TABLE IF NOT EXISTS `task_user` (
-  `task_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
