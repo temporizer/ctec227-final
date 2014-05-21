2381f960e97bb71dcb8915d47bf3ef19da8dfc98
temporizer <temporizer@gmail.com>
2014-05-20 21:37:57 -0700

    added gitignore

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php include 'includes/header.inc.php'; ?>
-<<<<<<< HEAD
+
 
 <?php login('j.conklin', 'password'); ?>
 
-<pre><?php print_r($_SESSION) ?></pre>
-=======
 <h1>Please log in to make changes</h1>
 <table></table>
->>>>>>> cac3ff8a6d5d89d8c3b27f698f3dbc3bad6bf36a
 
 <?php include 'includes/footer.inc.php'; ?>
