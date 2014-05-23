b0bb9a84bff46de31cda424c65af590c3fb1cabb
Cristi Jenkins <c.jenkin5@students.clark.edu>
2014-05-22 17:48:51 -0700

    created html for table in index.php

--- index.php
+++ index.php
@@ -4,6 +4,21 @@
 <?php login('j.conklin', 'password'); ?>
 
 <h1>Please log in to make changes</h1>
-<table></table>
+<table>
+<tr>
+  <td>Name</td>
+  <td>Project Manager</td>
+  <td>Start Date</td>
+  <td>Due Date</td>
+  <td>Status</td>
+</tr>
+<tr>
+  <td>Control Freakette</td>
+  <td>Mrs. Barnes</td>
+  <td>4/14/2014</td>
+  <td>5/15/2014</td>
+  <td>Active</td>
+</tr>
+</table>
 
 <?php include 'includes/footer.inc.php'; ?>
