511f7a330eaa256211337307a92700050877aff5
Cristi Jenkins <c.jenkin5@students.clark.edu>
2014-05-24 13:52:23 -0700

    changes to index.php and css
    I had to redo the table and put in th’s for headers instead of all td’s.
    I styled the table to at least get it looking somewhat functional with
    the help from one of my books.
    I have no idea that the rogue false is doing on the displayed page.

--- index.php
+++ index.php
@@ -3,20 +3,21 @@
 
 <?php login('j.conklin', 'password'); ?>
 
-<h1>Please log in to make changes</h1>
+<h1 class="mighty">List of Projects</h1>
+<h2>Please log in to see details and to make changes.</h2>
 <table>
 <tr>
-  <td>Name</td>
-  <td>Project Manager</td>
-  <td>Start Date</td>
-  <td>Due Date</td>
-  <td>Status</td>
+  <th>Project Name</th>
+  <th>Project Manager</th>
+  <th>Start Date</th>
+  <th>Due Date</th>
+  <th>Status</th>
 </tr>
 <tr>
-  <td>Control Freakette</td>
+  <td>I'm Watching U</td>
   <td>Mrs. Barnes</td>
   <td>4/14/2014</td>
-  <td>5/15/2014</td>
+  <td>5/15/2020</td>
   <td>Active</td>
 </tr>
 </table>
